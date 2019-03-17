f9804fe6a0449251c288da1e25414616959168a3
sasial-roblox <samuelh24c@gmail.com>
2019-03-17 10:37:00 +1000

    V0.1.0

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
-  };
+function createdb(dbname) {
+  if (typeof dbname !== "string") throw new TypeError("A string is needed!");
+  const express = require('express');
+  const app = express();
+  var db = require('quick.db')
+  var economy = new db.table(dbname)
+    await delay(1000)
+    app.get('/' + dbname, function(req, res){
+      db.fetch(dbname).then(function(db2) {
+      res.json(db2)
+      });
+  });
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.get("/" + dbname, (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+}
+function setdb(dbname,toadd) {
+  const express = require('express');
+  const app = express();
+  var db = require('quick.db')
+  const delay = require('delay');
+    const json = {content: toadd, placholder: "Do not remove"};
+    db.set(dbname, json)
+    await delay(1000)
+    app.get('/' + dbname, function(req, res){
+      db.fetch(dbname).then(function(db2) {
+      res.json(db2)
+      });
+  });
+}
+function resetdb(dbname) {
+  if (typeof dbname !== "string") throw new TypeError("A string is needed!");
+  const express = require('express');
+  const app = express();
+  var db = require('quick.db')
+  const delay = require('delay');
+  var economy = new db.table(dbname)
+  //Add stuff here
+  await delay(1000)
+  app.get('/' + dbname, function(req, res){
+    db.fetch(dbname).then(function(db2) {
+    res.json(db2)
+    });
+});
+}
+module.export.dbcreate = createddb;
+module.export.dbset = setdb;
+module.export.dbreset = resetdb;
