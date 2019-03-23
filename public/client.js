8bcbeac05771b66763b8029eceebc846ac038b54
sasial-roblox <samuelh24c@gmail.com>
2019-03-23 10:53:00 +1000

    0.1.21

--- public/client.js
+++ public/client.js
@@ -1,21 +1,23 @@
 const dreamsForm = document.forms[0];
 const dreamInput = dreamsForm.elements['dream'];
-const dbname = dreamsForm.elements['seter'];
-console.log(dreamInput.value)
-console.log(dbname.value)
+const dbnames = dreamsForm.elements['seter'];
+const dbname = dbnames.value
+const toadd = dreamInput.value
+console.log(toadd)
+console.log(dbname)
 
 dreamsForm.onsubmit = function(event) {
   event.preventDefault();
   const express = require('express');
   const app = express();
   var db = require('quick.db')
-    const json = {content: dreamInput.value, placeholder: "Do not remove"};
-    db.set(dbname.value, json)
-    app.get('/' + dbname.value, function(req, res){
-      db.fetch(dbname.value).then(function(db2) {
+  const json = {content: toadd, placeholder: "Do not remove"};
+  db.set(dbname, json)
+    app.get('/' + dbname, function(req, res){
+      var db2 = db.fetch(dbname)
+      console.log(db2)
       res.json(db2)
       });
-  });
   app.get('/', function(request, response) {
     response.sendFile(__dirname + '/views/index.html');
   });
