687ad0d42992c7db6b9e6fe3773f7d1c0666ad0e
sasial-roblox <samuelh24c@gmail.com>
2019-03-23 11:47:12 +1000

    0.1.24

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+  var whilei = false
+  var dbnamei
+  while (whilei == true) {
+    for (i = 1; i < dbnamei.length; i++){
+    app.get('/' + dbname[i], function(req, res){
+      var db2 = db.fetch(dbname[i])
+      console.log(db2)
+      res.json(db2)
+  });
+}
+  delay(5000)
+}
 async function createdb(dbname) {
   if (typeof dbname !== "string") throw new TypeError("A string is needed!");
   const express = require('express');
@@ -6,24 +18,27 @@ async function createdb(dbname) {
   var db = require('quick.db')
   var delay = require('delay')
   var economy = new db.table(dbname)
-  var whilei = true
-  while (whilei == true) {
-    app.get('/' + dbname, function(req, res){
-      var db2 = db.fetch(dbname)
-      console.log(db2)
-      res.json(db2)
-  });
-  delay(5000)
-}
+  whilei = true
+  dbnamei.push(dbname)
   app.get('/', function(request, response) {
     response.sendFile(__dirname + '/views/index.html');
   });
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+app.get('/' + dbname, function(req, res){
+  var db2 = db.fetch(dbname)
+  console.log(db2)
+  res.json(db2)
+});
 app.get("/" + dbname, (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
+});
+  app.get('/' + dbname, function(req, res){
+    var db2 = db.fetch(dbname)
+    console.log(db2)
+    res.json(db2)
 });
 }
 function setdb(dbname,toadd) {
