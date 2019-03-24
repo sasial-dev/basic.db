9385a736e4aa604a09d5e7376332c15b9b9f8f18
sasial-roblox <samuelh24c@gmail.com>
2019-03-24 16:05:44 +1000

    0.2.0

--- index.js
+++ index.js
@@ -1,62 +1,24 @@
-  var whilei = false
-  var dbnamei = ["place holder"]
-  while (whilei == true) {
-    print(dbnamei)
-    for (i = 1; i < dbnamei.length; i++){
-    app.get('/' + dbname[i], function(req, res){
-      var db2 = db.fetch(dbname[i])
-      console.log(db2)
-      res.json(db2)
-  });
-}
-  delayi(5000)
-}
-async function delayi(time) {
+  async function delayi(time) {
   await delay(time)
 }
 function createdb(dbname) {
   if (typeof dbname !== "string") throw new TypeError("A string is needed!");
-  const express = require('express');
-  const app = express();
-  app.use(express.static('public'));
   var db = require('quick.db')
   var delay = require('delay')
   var economy = new db.table(dbname)
   whilei = true
   dbnamei.push(dbname)
-  app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/views/index.html');
-  });
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.get("/" + dbname, (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
 }
 function setdb(dbname,toadd) {
   var db = require('quick.db')
     const json = {content: toadd, placeholder: "Do not remove"};
     db.set(dbname, json)
-      app.get('/' + dbname, function(req, res){
-        var db2 = db.fetch(dbname)
-        console.log(db2)
-        res.json(db2)
-        });
 }
 function resetdb(dbname) {
   if (typeof dbname !== "string") throw new TypeError("A string is needed!");
-  const express = require('express');
-  const app = express();
   var db = require('quick.db')
   const json = {};
   db.set(dbname, json)
-    app.get('/' + dbname, function(req, res){
-      var db2 = db.fetch(dbname)
-      console.log(db2)
-      res.json(db2)
-      });
 }
 module.exports.dbcreate = createdb;
 module.exports.dbset = setdb;
